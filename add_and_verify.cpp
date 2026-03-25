a1c2d8bb0c85ac73a36c5b1a89fb3e1f723aa51d
Kyle Knoepfel <knoepfel@fnal.gov>
2026-03-25 15:01:11 -0500

    Accommodate changes to C++23 and product specification

--- add_and_verify.cpp
+++ add_and_verify.cpp
@@ -10,9 +10,10 @@ PHLEX_REGISTER_ALGORITHMS(m, config)
   auto const layer = config.get<std::string>("layer");
 
   m.transform("add", examples::add, concurrency::unlimited)
-    .input_family("i"_in(layer), "j"_in(layer))
-    .output_products("sum");
+    .input_family(product_query{.creator = "input", .suffix = "i", .layer = layer},
+                  product_query{.creator = "input", .suffix = "j", .layer = layer})
+    .output_product_suffixes("sum");
   m.observe(
      "verify", [](int actual) { assert(actual == 0); }, concurrency::unlimited)
-    .input_family("sum"_in(layer));
+    .input_family(product_query{.creator = "add", .suffix = "sum", .layer = layer});
 }
--- numbers_source.cpp
+++ numbers_source.cpp
@@ -9,7 +9,7 @@ PHLEX_REGISTER_PROVIDERS(m, config)
   auto const layer = config.get<std::string>("layer");
 
   m.provide("provide_i", [](data_cell_index const& id) -> int { return id.number(); })
-    .output_product("i"_in(layer));
+    .output_product({.creator = "input", .suffix = "i", .layer = layer});
   m.provide("provide_j", [](data_cell_index const& id) -> int { return -id.number(); })
-    .output_product("j"_in(layer));
+    .output_product({.creator = "input", .suffix = "j", .layer = layer});
 }
